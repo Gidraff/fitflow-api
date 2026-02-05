ac866ad0373fb4cd0f632f802cd3709d3839637b
Gidraff <kamandegid@gmail.com>
2026-02-05 17:02:57 +0300

    debug: add options headers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -76,11 +76,6 @@ func main() {
 
 	// 4. Route Definitions
 	r.Route("/api/v1", func(r chi.Router) {
-		// Public Routes (if any)
-		r.Options("/*", func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusNoContent)
-		})
-
 		r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte("OK"))
@@ -88,6 +83,13 @@ func main() {
 
 		// Protected Routes
 		r.Group(func(r chi.Router) {
+			r.Use(func(next http.Handler) http.Handler {
+				return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.Header().Add("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+					w.Header().Add("Access-Control-Allow-Credentials", "true")
+					next.ServeHTTP(w, r)
+				})
+			})
 			r.Use(auth.AuthMiddleware(jwksURL, issuer))
 
 			// Client Endpoints
